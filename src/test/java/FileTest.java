bda033d9527e3001744c88d78808e028fa0ccc3d
nyashin alex <Nyashin-AV@ao-rr.rrgit config --global user.email Nyashin-AV@ao-rr.rr>
2022-10-05 18:54:21 +0300

    start homework

--- src/test/java/FileTest.java
+++ src/test/java/FileTest.java
@@ -1,7 +1,11 @@
+import com.opencsv.CSVReader;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
@@ -13,28 +17,17 @@ public class FileTest {
     @Test
     void zipTest() throws Exception {
 
-//        try (InputStream is = cl.getResourceAsStream("exemple_zip.zip")) {
-//            assert is != null;
-//            try (ZipInputStream zis = new ZipInputStream(is)) {
-//                ZipEntry entry;
-//
-//                while ((entry = zis.getNextEntry()) != null) {
-//                    String entryName = entry.getName();
-////                    byte[] fileSource = is.readAllBytes();
-////                    InputStream isFile = cl.getResourceAsStream(file);
-//                    System.out.println(entryName);
-//
-//                }
-//            }
-//        }
-        ZipFile zf = new ZipFile(new File("src/test/resources/exemple_zip.zip"));
-        ZipInputStream is = new ZipInputStream(cl.getResourceAsStream("exemple_zip.zip"));
+        ZipFile zf = new ZipFile(new File("src/test/resources/example_csv.zip"));
+        ZipInputStream is = new ZipInputStream(cl.getResourceAsStream("example_csv.zip"));
         ZipEntry entry;
         while((entry = is.getNextEntry()) != null) {
             org.assertj.core.api.Assertions.assertThat(entry.getName()).isEqualTo(entry.getName());
-            try (InputStream inputStream = zf.getInputStream(entry)) {
-                // проверки/
-                System.out.println("");
+            try (InputStream inputStream = zf.getInputStream(entry);
+                 CSVReader reader = new CSVReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+                List<String[]> content = reader.readAll();
+                String[] x = content.get(0);
+                String y = x[0];
+                System.out.println(y);
             }
         }
     }

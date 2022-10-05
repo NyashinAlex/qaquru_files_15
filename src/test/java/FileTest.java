d1244a152578dbd8e0282c13edf81246fb4228fc
nyash <alex-nyashin1996@yandex.ru>
2022-10-05 23:19:13 +0300

    -

--- src/test/java/FileTest.java
+++ src/test/java/FileTest.java
@@ -1,4 +1,8 @@
+import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
+import com.monitorjbl.xlsx.StreamingReader;
 import com.opencsv.CSVReader;
+import net.sf.jxls.transformer.Workbook;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -10,6 +14,8 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class FileTest {
 
     ClassLoader cl = FileTest.class.getClassLoader();
@@ -17,17 +23,40 @@ public class FileTest {
     @Test
     void zipTest() throws Exception {
 
-        ZipFile zf = new ZipFile(new File("src/test/resources/example_csv.zip"));
-        ZipInputStream is = new ZipInputStream(cl.getResourceAsStream("example_csv.zip"));
+        ZipFile zf = new ZipFile(new File("src/test/resources/example.zip"));
+        ZipInputStream is = new ZipInputStream(cl.getResourceAsStream("example.zip"));
         ZipEntry entry;
         while((entry = is.getNextEntry()) != null) {
-            org.assertj.core.api.Assertions.assertThat(entry.getName()).isEqualTo(entry.getName());
-            try (InputStream inputStream = zf.getInputStream(entry);
-                 CSVReader reader = new CSVReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
-                List<String[]> content = reader.readAll();
-                String[] x = content.get(0);
-                String y = x[0];
-                System.out.println(y);
+//            org.assertj.core.api.Assertions.assertThat(entry.getName()).isEqualTo(entry.getName());
+            if (entry.getName().equals("example_csv.csv")) {
+                try (InputStream inputStream = zf.getInputStream(entry);
+                     CSVReader reader = new CSVReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+                    List<String[]> content = reader.readAll();
+                    String[] x = content.get(0);
+                    String y = x[0];
+                    System.out.println(y);
+                }
+            } else if (entry.getName().equals("example_xlsx.xlsx")) {
+                try (InputStream inputStream = zf.getInputStream(entry)){
+//                    Workbook workbook = StreamingReader.builder()
+//                            .rowCacheSize(0)
+//                            .bufferSize(0)
+//                            .open(inputStream);
+
+                    XLS xls = new XLS(inputStream);
+//                    assertThat(
+//                            xls.excel.getSheetAt(0)
+//                                    .getRow(5)
+//                                    .getCell(5)
+//                                    .getStringCellValue()
+//                    ).isEqualTo("Dulce");
+                    System.out.println("xls");
+                }
+            } else if (entry.getName().equals("example_pdf.pdf")) {
+                try (InputStream inputStream = zf.getInputStream(entry)){
+                    PDF pdf = new PDF(inputStream);
+                    System.out.println("pdf");
+                }
             }
         }
     }

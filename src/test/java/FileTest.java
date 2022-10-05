879e996f18c5541c56719ef982310f55e804c06b
nyash <alex-nyashin1996@yandex.ru>
2022-10-06 00:45:10 +0300

    add full work with zip-file

--- src/test/java/FileTest.java
+++ src/test/java/FileTest.java
@@ -1,8 +1,6 @@
 import com.codeborne.pdftest.PDF;
 import com.codeborne.xlstest.XLS;
-import com.monitorjbl.xlsx.StreamingReader;
 import com.opencsv.CSVReader;
-import net.sf.jxls.transformer.Workbook;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -28,35 +26,33 @@ public class FileTest {
         ZipEntry entry;
         while((entry = is.getNextEntry()) != null) {
 //            org.assertj.core.api.Assertions.assertThat(entry.getName()).isEqualTo(entry.getName());
-            if (entry.getName().equals("example_csv.csv")) {
-                try (InputStream inputStream = zf.getInputStream(entry);
-                     CSVReader reader = new CSVReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
-                    List<String[]> content = reader.readAll();
-                    String[] x = content.get(0);
-                    String y = x[0];
-                    System.out.println(y);
-                }
-            } else if (entry.getName().equals("example_xlsx.xlsx")) {
-                try (InputStream inputStream = zf.getInputStream(entry)){
-//                    Workbook workbook = StreamingReader.builder()
-//                            .rowCacheSize(0)
-//                            .bufferSize(0)
-//                            .open(inputStream);
-
-                    XLS xls = new XLS(inputStream);
-//                    assertThat(
-//                            xls.excel.getSheetAt(0)
-//                                    .getRow(5)
-//                                    .getCell(5)
-//                                    .getStringCellValue()
-//                    ).isEqualTo("Dulce");
-                    System.out.println("xls");
-                }
-            } else if (entry.getName().equals("example_pdf.pdf")) {
-                try (InputStream inputStream = zf.getInputStream(entry)){
-                    PDF pdf = new PDF(inputStream);
-                    System.out.println("pdf");
-                }
+            switch (entry.getName()) {
+                case "example_csv.csv":
+                    try (InputStream inputStream = zf.getInputStream(entry);
+                         CSVReader reader = new CSVReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+                        List<String[]> content = reader.readAll();
+                        String[] row = content.get(0);
+                        String searchWords = row[7];
+                        assertThat(searchWords).isEqualTo("Variable_category");
+                    }
+                    break;
+                case "example_xlsx.xlsx":
+                    try (InputStream inputStream = zf.getInputStream(entry)) {
+                        XLS xls = new XLS(inputStream);
+                        assertThat(
+                                xls.excel.getSheetAt(0)
+                                        .getRow(5)
+                                        .getCell(4)
+                                        .getStringCellValue()
+                        ).isEqualTo("United States");
+                    }
+                    break;
+                case "example_pdf.pdf":
+                    try (InputStream inputStream = zf.getInputStream(entry)) {
+                        PDF pdf = new PDF(inputStream);
+                        assertThat(pdf.author).isEqualTo("Alex");
+                    }
+                    break;
             }
         }
     }

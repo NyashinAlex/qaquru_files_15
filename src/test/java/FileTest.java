f2f40287101d60097a2f8f278f08ff60cb01665a
nyashin alex <Nyashin-AV@ao-rr.rrgit config --global user.email Nyashin-AV@ao-rr.rr>
2022-10-06 10:18:50 +0300

    fix teacher comments :)

--- src/test/java/FileTest.java
+++ src/test/java/FileTest.java
@@ -24,36 +24,37 @@ public class FileTest {
     void zipTest() throws Exception {
 
         ZipFile zf = new ZipFile(new File("src/test/resources/example.zip"));
-        ZipInputStream is = new ZipInputStream(cl.getResourceAsStream("example.zip"));
-        ZipEntry entry;
-        while((entry = is.getNextEntry()) != null) {
-            switch (entry.getName()) {
-                case "example_csv.csv":
-                    try (InputStream inputStream = zf.getInputStream(entry);
-                         CSVReader reader = new CSVReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
-                        List<String[]> content = reader.readAll();
-                        String[] row = content.get(0);
-                        String searchWords = row[7];
-                        assertThat(searchWords).isEqualTo("Variable_category");
-                    }
-                    break;
-                case "example_xlsx.xlsx":
-                    try (InputStream inputStream = zf.getInputStream(entry)) {
-                        XLS xls = new XLS(inputStream);
-                        assertThat(
-                                xls.excel.getSheetAt(0)
-                                        .getRow(5)
-                                        .getCell(4)
-                                        .getStringCellValue()
-                        ).isEqualTo("United States");
-                    }
-                    break;
-                case "example_pdf.pdf":
-                    try (InputStream inputStream = zf.getInputStream(entry)) {
-                        PDF pdf = new PDF(inputStream);
-                        assertThat(pdf.author).isEqualTo("Alex");
-                    }
-                    break;
+        try (ZipInputStream is = new ZipInputStream(cl.getResourceAsStream("example.zip"))) {
+            ZipEntry entry;
+            while ((entry = is.getNextEntry()) != null) {
+                switch (entry.getName()) {
+                    case "example_csv.csv":
+                        try (InputStream inputStream = zf.getInputStream(entry);
+                             CSVReader reader = new CSVReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+                            List<String[]> content = reader.readAll();
+                            String[] row = content.get(0);
+                            String searchWords = row[7];
+                            assertThat(searchWords).isEqualTo("Variable_category");
+                        }
+                        break;
+                    case "example_xlsx.xlsx":
+                        try (InputStream inputStream = zf.getInputStream(entry)) {
+                            XLS xls = new XLS(inputStream);
+                            assertThat(
+                                    xls.excel.getSheetAt(0)
+                                            .getRow(5)
+                                            .getCell(4)
+                                            .getStringCellValue()
+                            ).isEqualTo("United States");
+                        }
+                        break;
+                    case "example_pdf.pdf":
+                        try (InputStream inputStream = zf.getInputStream(entry)) {
+                            PDF pdf = new PDF(inputStream);
+                            assertThat(pdf.author).isEqualTo("Alex");
+                        }
+                        break;
+                }
             }
         }
     }
